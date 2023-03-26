9cab9d08033628d0a38fca397fc84235115bcbda
Rob Scherer <robscherer6@gmail.com>
2023-03-26 08:09:09 -0400

    added input validation to require user input, no blank form submission

--- app.js
+++ app.js
@@ -1,22 +1,36 @@
+const itemForm = document.getElementById('item-form');
+
 //create reference to input text
 let itemInput = document.getElementById('item-input');
 
 //create reference to addItem button
-let addItem = document.querySelector('.btn');
+//let addItem = document.querySelector('.btn');
 
 //create reference to item list
-let ul = document.getElementById('item-list');
+let itemList = document.getElementById('item-list');
 
-function onSubmit (e) {
+function addItem (e) {
   //prevent defaul submission
   e.preventDefault();
-  let li = document.createElement('li');
-  li.textContent = itemInput.value;
-  li.className = 
 
-  ul.appendChild(li);
+  //Validate Input
+  if (itemInput.value === '') {
+    alert('Please add an item');
+    return;
+  };
+
+  console.log('Success!');
+
+  // let li = document.createElement('li');
+  // li.textContent = itemInput.value;
+  // li.className =
+
+  // itemList.appendChild(li);
 }
 
-//add event listener to addItem
-addItem.addEventListener('click', onSubmit)
+
+
+//Event Listeners
+itemForm.addEventListener('submit', addItem);
+//addItem.addEventListener('click', onSubmit)
 
